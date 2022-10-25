cd359b61a117b9e89b105bb58efcd6b896547103
Meera <116206512+meeragitdev@users.noreply.github.com>
2022-10-26 00:24:53 +0530

    Update streamlit_app.py
    fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ streamlit.dataframe(fruits_to_show)
 
 #create function
 def get_fruityvice_data(this_fruit_choice):
-    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)   
+    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice) 
+    streamlit.write('The fruit entered ', this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
     return fruityvice_normalized  
 
